41a270e5aa0e1922f065a600e98432bf1fa2a9d6
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2015-11-28 15:15:30 -0800

    added files

---
+++ odm_meshing_win/CMakeFiles/odm_meshing.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/dev/odm_meshing_cyg/src/Logger.cpp" "/cygdrive/c/dev/odm_meshing_cyg/CMakeFiles/odm_meshing.dir/src/Logger.cpp.o"
+  "/cygdrive/c/dev/odm_meshing_cyg/src/OdmMeshing.cpp" "/cygdrive/c/dev/odm_meshing_cyg/CMakeFiles/odm_meshing.dir/src/OdmMeshing.cpp.o"
+  "/cygdrive/c/dev/odm_meshing_cyg/src/main.cpp" "/cygdrive/c/dev/odm_meshing_cyg/CMakeFiles/odm_meshing.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/include/pcl-."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ odm_meshing_win/CMakeFiles/odm_meshing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odm_meshing.dir/src/Logger.cpp.o"
+  "CMakeFiles/odm_meshing.dir/src/main.cpp.o"
+  "CMakeFiles/odm_meshing.dir/src/OdmMeshing.cpp.o"
+  "odm_meshing.pdb"
+  "odm_meshing.exe"
+  "libodm_meshing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odm_meshing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
